cacae27456e8fcba55817477c2e7a717d0bd6b95
Abel Begashaw <abelbegashawmekuria@gmail.com>
2026-08-30 23:08:46 +0300

    Time: 12 ms (88.76%), Space: 34.3 MB (5.33%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.py
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.py
@@ -8,7 +8,7 @@ class Solution:
             max_index, min_index = min_index, max_index
 
         return min (
-            max(min_index, max_index) + 1,
-            len(nums) - min(min_index, max_index),
+            max_index + 1,
+            len(nums) - min_index,
             min_index + 1 + len(nums) - max_index,
         )
